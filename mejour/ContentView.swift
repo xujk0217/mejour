1d9fd06583c8fb4b778916eac28a17606be43b7b
許君愷 / xu jun kai <kevin16021777@gmail.com>
2025-10-09 18:46:01 +0800

    Initial Commit

---
+++ mejour/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mejour
+//
+//  Created by 許君愷 on 2025/10/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mejour/mejourApp.swift
@@ -0,0 +1,17 @@
+//
+//  mejourApp.swift
+//  mejour
+//
+//  Created by 許君愷 on 2025/10/9.
+//
+
+import SwiftUI
+
+@main
+struct mejourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
